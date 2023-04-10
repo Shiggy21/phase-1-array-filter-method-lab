5a7b5339a86124dab0f1fb43807d64abd7fbe72b
Shane Jampole <shane.jampole.98@gmail.com>
2023-04-09 20:19:13 -0400

    committing labwork

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+const findMatching = (drivers, string) => {
+   return drivers.filter(drivers => drivers.toUpperCase() === string.toUpperCase())
+}
+
+const fuzzyMatch = (name, string) => {
+    return name.filter(name => name.charAt(0) === string.charAt(0))
+}
+
+const matchName = (driver, string) => { 
+    return driver.filter(driver => driver.name === string)
+}
